14e137c97a8744a6147399a9838055789e90968c
ankama-ad <ankamma.bollimuntha@amnetdigital.com>
2021-06-18 11:19:57 +0530

    added api for user reports

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -2,15 +2,23 @@
 // const PermissionMiddleware = require('../common/middlewares/auth.permission.middleware');
 //const ValidationMiddleware = require('../common/middlewares/auth.validation.middleware');
 // const config = require('../common/config/env.config');
-const powerbiapi = require('../api/reportAPI');
+const reportapi = require('../api/reportAPI');
 const express = require("express");
 const router = express.Router();
 
 router.get('/getReports', [
     //ValidationMiddleware.validJWTNeeded,
     // PermissionMiddleware.minimumPermissionLevelRequired(PAID),
-    powerbiapi.getReports
+    reportapi.getReports
 
 ]);
+
+router.get('/getUserReports', [
+    //ValidationMiddleware.validJWTNeeded,
+    // PermissionMiddleware.minimumPermissionLevelRequired(PAID),
+    reportapi.getUserReports
+
+]);
+
 
 module.exports = router;
