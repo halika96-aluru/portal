38f0942eaf747c0bec2b14559b140ba780deee21
Ankamma Bollimuntha <ankamma.bollimuntha@cdw.com>
2021-07-28 12:43:58 -0700

    build issue fixed

--- src/app/views/configurations/settings/user-role-settings/user-role-settings.component.ts
+++ src/app/views/configurations/settings/user-role-settings/user-role-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-role-settings',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRoleSettingsComponent implements OnInit {
 
-  constructor() { }
+  activeTabIndex = 0;
+  constructor(private router: Router) {
+    if(this.router.getCurrentNavigation().extras.state) {
+      this.activeTabIndex = <number>this.router.getCurrentNavigation().extras.state.activeTabIndex;
+    }
+   }
 
   ngOnInit(): void {
   }
